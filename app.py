953f84d5cb6a1644e24dc2b07e0d99e3dc12468e
jordan-hernandez <jordan.hernandez@udea.edu.co>
2024-05-27 11:31:20 -0500

    cambio dimensión

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ dk_["date.utc"] = dk_["date.utc"].apply(lambda x: calendar.month_abbr[int(x)])
 # Crear la gráfica de barras con Plotly Express
 fig = px.bar(dk_, x='date.utc', y=parameter, color='location', barmode='group',
              labels={"date.utc": "Mes", parameter: f" Concentración  {parameter}  (&mu;g/m<sup>3</sup>)"},
-             width=1500,  # Ancho de la gráfica en píxeles
+             width=2000,  # Ancho de la gráfica en píxeles
              height=450   # Altura de la gráfica en píxeles
              )
 # Mostrar la gráfica de barras en Streamlit
@@ -84,7 +84,7 @@ l = selected_df.groupby([selected_df["date.utc"].dt.hour, pd.Grouper(key="locati
 # Crear la gráfica de línea con Plotly Express
 fig2 = px.line(l.reset_index(), x='date.utc', y=parameter, color='location',
                labels={"date.utc": "Horas del día", parameter: f"Concentración {parameter}(&mu;g/m<sup>3</sup>)"},
-               width=1500,  # Ancho de la gráfica en píxeles
+               width=2000,  # Ancho de la gráfica en píxeles
                height=450   # Altura de la gráfica en píxeles
                )
 
