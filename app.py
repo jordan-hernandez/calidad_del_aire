df5592301a7864ae296c519ad40fb97670aec4a8
jordan-hernandez <jordan.hernandez@udea.edu.co>
2024-05-27 13:22:15 -0500

    indicadores

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(layout="wide")
 st.title("Calidad del aire en Medellín")
 
 # Sidebar
-st.sidebar.title("Indicadores")
+st.sidebar.title("Opciones")
 # Añadir una imagen a la sidebar
 st.sidebar.image("./calidad_indicadores.png",use_column_width=True)
 
