1f452c8ca0c7bbe072369dbccfb43333f0d00b42
jordan-hernandez <jordan.hernandez@udea.edu.co>
2024-05-27 13:07:49 -0500

    cambio dimensión

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ st.set_page_config(layout="wide")
 # Título principal
 st.title("Calidad del aire en Medellín")
 
+# Sidebar
+st.sidebar.title("Opciones")
+# Añadir una imagen a la sidebar
+st.sidebar.image("./src/calidad_indicadores.png", use_column_width=True)
+
 # Menú desplegable para seleccionar el año
 selected_year = st.selectbox("Seleccionar año", list(dataframes.keys()))
 
